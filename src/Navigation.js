b137265ee2a87d73f1a37697b9d3fad1d8649eb0
Richelle Soares <richelle.soares@gmail.com>
2021-01-17 18:21:39 -0300

    Inserir as navegações

--- src/Navigation.js
+++ src/Navigation.js
@@ -10,9 +10,42 @@ const Tab = createBottomTabNavigator();
 export default function Navigation(){
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Wallet" component={WalletScreen} />
-            <Tab.Screen name="Pay" component={PayScreen} />
+            <Tab.Screen 
+            name="Home" 
+            component={HomeScreen} 
+            options={{
+                title: 'Início'
+            }}
+            />
+            <Tab.Screen 
+            name="Wallet" 
+            component={WalletScreen} 
+            options={{
+                title: 'Carteira'
+            }}
+            />
+            
+            <Tab.Screen 
+            name="Pay" 
+            component={PayScreen} 
+            options={{
+                title: 'Pagar'
+            }}
+            />
+            <Tab.Screen 
+            name="Notification" 
+            component={PayScreen} 
+            options={{
+                title: 'Notificações'
+            }}
+            />
+            <Tab.Screen 
+            name="Settings" 
+            component={PayScreen} 
+            options={{
+                title: 'Ajustes'
+            }}
+            />
         </Tab.Navigator>
 
     );
