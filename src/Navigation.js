6917750777a52feb367a5ee37917811fa83779d8
Richelle Soares <richelle.soares@gmail.com>
2021-01-17 17:41:59 -0300

    Acrescentando a parte de Navegação das páginas

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './screen/Home';
+import WalletScreen from './screen/Wallet';
+import PayScreen from './screen/Pay';
+
+
+const Tab = createBottomTabNavigator();
+export default function Navigation(){
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Wallet" component={WalletScreen} />
+            <Tab.Screen name="Pay" component={PayScreen} />
+        </Tab.Navigator>
+
+    );
+}
---
+++ src/screen/Home/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Text } from 'react-native';
+export default function Home(){
+    return <Text>Home</Text>;
+}
